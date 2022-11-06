4d446bdb6931656d98f0f32c75902166bfa66cf8
David Sun <davidthesun1001@gmail.com>
2022-11-06 00:01:43 -0400

    adding event handler function

--- api.py
+++ api.py
@@ -9,26 +9,28 @@ from tgtg import TgtgClient
 import json
 from pprint import pprint
 
-f = open('tgtg-tokens.json')
+def tgtg_hander(event, context):
+    f = open('tgtg-tokens.json')
 
-data = json.load(f)
+    data = json.load(f)
 
-client = TgtgClient(access_token=data['accessToken'], 
-                    refresh_token=data['refreshToken'], 
-                    user_id=data['userID'])
+    client = TgtgClient(access_token=data['accessToken'], 
+                        refresh_token=data['refreshToken'], 
+                        user_id=data['userID'])
 
-# You can then get some items, by default it will *only* get your favorites
-# items = client.get_items()
-# print(items)
+    # You can then get some items, by default it will *only* get your favorites
+    # items = client.get_items()
+    # print(items)
 
-# To get items (not only your favorites) you need to provide location informations
-items = client.get_items(
-    favorites_only=True,
-    latitude=40.74793817710335,
-    longitude=-73.9423128698922,
-    radius=10,
-)
+    # To get items (not only your favorites) you need to provide location informations
+    items = client.get_items(
+        favorites_only=True,
+        latitude=40.74793817710335,
+        longitude=-73.9423128698922,
+        radius=10,
+    )
 
+    print(items[0]['display_name'])
+    print(items[0]['items_available'])
 
-print(items[0]['display_name'])
-print(items[0]['items_available'])
+    return items[0]['items_available']
