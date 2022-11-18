e054012f5a9f6604a86780da05b2b57800106ecd
David Sun <davidthesun1001@gmail.com>
2022-11-17 22:07:09 -0500

    adding readme

--- api.py
+++ api.py
@@ -9,7 +9,7 @@ from tgtg import TgtgClient
 import json
 from pprint import pprint
 
-def tgtg_hander(event, context):
+def tgtg_handler(event, context):
     f = open('tgtg-tokens.json')
 
     data = json.load(f)
@@ -30,7 +30,35 @@ def tgtg_hander(event, context):
         radius=10,
     )
 
-    print(items[0]['display_name'])
-    print(items[0]['items_available'])
+    print(items[1]['display_name'])
+    print(items[1]['items_available'])
 
-    return items[0]['items_available']
+    return items[1]['items_available']
+
+def standalone():
+    f = open('tgtg-tokens.json')
+
+    data = json.load(f)
+
+    client = TgtgClient(access_token=data['accessToken'], 
+                        refresh_token=data['refreshToken'], 
+                        user_id=data['userID'])
+
+    # You can then get some items, by default it will *only* get your favorites
+    # items = client.get_items()
+    # print(items)
+
+    # To get items (not only your favorites) you need to provide location informations
+    items = client.get_items(
+        favorites_only=True,
+        latitude=40.74793817710335,
+        longitude=-73.9423128698922,
+        radius=10,
+    )
+
+    print(items[1]['display_name'])
+    print(items[1]['items_available'])
+
+    return items[1]['items_available']
+if __name__ == "__main__":
+    standalone()
